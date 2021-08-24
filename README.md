87e7831a42550d64f774f8633fbd3c6006269bfd
ferdousz <jannatrumi60@gmail.com>
2021-08-25 03:57:35 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mac-book-pro-ferdousz
+# assignment-5-mac-book
--- js file/index.js
+++ js file/index.js
@@ -1,96 +1,119 @@
-// "Function" For Increase and Decrease Price Value/////////////////
-function getInputValue(inputId, amount) {
-    const inputField = document.getElementById(inputId);
-    const inputAmountText = inputField.innerText;
-    const amountValue = parseInt(inputAmountText);
-    inputField.innerText = amount;
-    return amountValue;
-}
-////////////// "Function For Update Balance"//////////////
-function getCurrentBalance() {
-    const balanceTotal = document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseInt(balanceTotalText);
-    return previousBalanceTotal;
-}
-function getCurrentsub() {
-    const endTotal = document.getElementById('end-total');
-    const endTotalText = endTotal.innerText;
-    const previousendTotal = parseInt(endTotalText);
-    return previousendTotal;
-}
-function updateBalance(amountValue, isAdd) {
-    const balanceTotal = document.getElementById('balance-total');
-    const TotalEnd = document.getElementById('end-total');
-    const previousendTotal = getCurrentsub();
-    const previousBalanceTotal = getCurrentBalance();
-    if (isAdd == true) {
-        balanceTotal.innerText = previousBalanceTotal + amountValue;
-        TotalEnd.innerText = previousendTotal + amountValue;
-    }
-    else {
-        balanceTotal.innerText = previousBalanceTotal - amountValue;
-        TotalEnd.innerText = previousendTotal - amountValue;
-    }
-}
-/////////////// Adding Price Value ///////////////////////
-document.getElementById('update-memory').addEventListener('click', function () {
-    const depositAmount = getInputValue('memory-price', 180);
-    updateBalance(180, true);
-});
-document.getElementById('update-storage').addEventListener('click', function () {
-    const depositAmount = getInputValue('storage-price', 100);
-    updateBalance(100, true);
-});
-document.getElementById('super-storage').addEventListener('click', function () {
-    const depositAmount = getInputValue('storage-price', 180);
-    updateBalance(180, true);
-});
-
-document.getElementById('update-delivery').addEventListener('click', function () {
-    const depositAmount = getInputValue('delivery-price', 20);
-    updateBalance(20, true);
-});
+const memoryDefault = document.getElementById('memory-default');
+const storageDefault = document.getElementById('storage-default');
+const deliveryDefault = document.getElementById('delivery-default');
 
 
-///////////////// "Function" For Decrease Price Value ///////////////
+const updateMemory = document.getElementById('update-memory');
+const updateStorage = document.getElementById('update-storage');
+const superStorage = document.getElementById('super-storage');
+const updateDelivery = document.getElementById('update-delivery');
 
-function decreseAmmount(service, ammount) {
-    const decresePrice = getInputValue(service + '-price', ammount);
-    const currentBalance = getCurrentBalance();
-    if (decresePrice < currentBalance) {
 
-        updateBalance(decresePrice, false);
-    }
-}
-/////////////// Decrease Price Value ///////////////////////
-document.getElementById('memory-default').addEventListener('click', function () {
-    decreseAmmount('memory', 0);
+const memoryPrice = document.getElementById('memory-price');
+const storagePrice = document.getElementById('storage-price');
+const deliveryPrice = document.getElementById('delivery-price');
+const bestPrice = document.getElementById('best-price');
+const endTotal = document.getElementById('end-total');
+
+
+
+
+
+const balanceTotal = document.getElementById('balance-total');
+
+memoryDefault.addEventListener('click',function(){
+    memoryPrice.innerText = 0 ;
+    decreaseBalance ()
+    
 })
-document.getElementById('storage-default').addEventListener('click', function () {
-    decreseAmmount('storage', 0);
+storageDefault.addEventListener('click',function(){
+    storagePrice.innerText = 0 ;
+    decreaseBalance ()
+    
 })
-document.getElementById('delivery-default').addEventListener('click', function () {
-    decreseAmmount('delivery', 0);
+deliveryDefault.addEventListener('click',function(){
+    deliveryPrice.innerText = 0 ;
+    decreaseBalance ()
+    
+})
+ 
+updateMemory.addEventListener('click', function(){
+    memoryPrice.innerText = 120 ;
+    increaseBalance ()
+
+})
+updateStorage.addEventListener('click', function(){
+    storagePrice.innerText = 200 ;
+    increaseBalance ()
+
+})
+superStorage.addEventListener('click', function(){
+    storagePrice.innerText = 300 ;
+    increaseBalance ()
+
+})
+updateDelivery.addEventListener('click', function(){
+    deliveryPrice.innerText = 10 ;
+    increaseBalance ()
+
 })
 
-//////////////////////////////////------------------BONUS----------------------\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-//////////////////////////////// Coupon Enable /////////////////////////////////
-document.getElementById('coupon-input').addEventListener('keyup', function (e) {
-    const couponBtn = document.getElementById('coupon');
-    if (e.target.value == 'stevekaku') {
+
+
+
+
+
+function decreaseBalance (){
+    
+    const bastPr = Number(bestPrice.innerText);
+    const memoryPr = Number(memoryPrice.innerText);
+    const storagePr = Number(storagePrice.innerText);
+    const deliveryPr = Number(deliveryPrice.innerText);
+    
+    balanceTotal.innerText = bastPr - (memoryPr + storagePr + deliveryPr);
+   endTotal.innerText = bastPr - (memoryPr + storagePr + deliveryPr);
+
+}
+
+function increaseBalance () {
+   
+    const bastPr = Number(bestPrice.innerText);
+    const memoryPr = Number(memoryPrice.innerText);
+    const storagePr = Number(storagePrice.innerText);
+    const deliveryPr = Number(deliveryPrice.innerText);
+    const totalBal = memoryPr + bastPr +storagePr +deliveryPr;
+    
+   balanceTotal.innerText = totalBal;
+   endTotal.innerText = totalBal;
+}
+
+const inputCoupon = document.getElementById('coupon-input');
+const couponBtn = document.getElementById('coupon');
+
+
+
+
+inputCoupon.addEventListener('keyup',function (event){
+    if (event.target.value ==  'hi'){
         couponBtn.removeAttribute('disabled');
+        
+       
     }
-    else {
-        couponBtn.setAttribute('disabled', true);
+    else{
+        couponBtn.setAttribute('disabled',true)
     }
-
 });
-//////////////////////////////// Coupon Remainder /////////////////////////////////
-document.getElementById('coupon').addEventListener('click', function () {
-    const endTotal = document.getElementById('end-total');
-    const previousendTotal = getCurrentsub();
-    endTotal.innerText = previousendTotal - previousendTotal * 20 / 100;
-
-});
+couponBtn.addEventListener('click',function(){
+    if (inputCoupon.value == 'hi'){
+        discount ();
+        inputCoupon.value = '';
+        
+    }
+ })
+ 
+ function discount (){
+    const endNum = Number(endTotal.innerText);
+    const endDisc = endNum - (endNum / 20);
+    endTotal.innerText = endDisc;
+ }
